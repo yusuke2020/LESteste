50cee21b62c9d5595a465ee1b1375fa4843b687e
Yusuke <kiyoshinguji@gmail.com>
2023-03-16 00:21:37 -0300

    início do sistema de crud

--- src/pages/CadastroProduto.jsx
+++ src/pages/CadastroProduto.jsx
@@ -1,16 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/styleCadastro.css";
+import { Header } from "../components/header";
 
 export default function CadastroProduto() {
-  const newLocal =
-    ((<div>Cadastro de Produtos</div>),
-    (
-      <div>
-        <input type="text" name="nome" placeholder="Nome do Produto" />
-        <input type="text" name="valor" placeholder="Preço" />
-        <input type="text" name="categoria" placeholder="Categoria" />
-        <input type="text" name="qtde" placeholder="Quantidade" />
-        <button>Cadastrar</button>
+  const [valores, setValores] = useState();
+  const mudarValores = (valor) => {
+    setValores((valorAnt) => ({
+      ...valorAnt,
+      [valor.target.name]: valor.target.value,
+    }));
+  };
+
+  const aoClicarBtnCadastro = () => {
+    console.log(valores);
+  };
+
+  const newLocal = (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="containerCadastro">
+          <h1>Cadastro de produtos</h1>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Nome do Produto"
+            className="inputCadastro"
+            onChange={mudarValores}
+          />
+          <input
+            type="text"
+            name="valor"
+            placeholder="Preço"
+            className="inputCadastro"
+            onChange={mudarValores}
+          />
+          <input
+            type="text"
+            name="categoria"
+            placeholder="Categoria"
+            className="inputCadastro"
+            onChange={mudarValores}
+          />
+          <input
+            type="text"
+            name="qtde"
+            placeholder="Quantidade"
+            className="inputCadastro"
+            onChange={mudarValores}
+          />
+          <button onClick={() => aoClicarBtnCadastro()}>Cadastrar</button>
+        </div>
       </div>
-    ));
+    </div>
+  );
   return newLocal;
 }
